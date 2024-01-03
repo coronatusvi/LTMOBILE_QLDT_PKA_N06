11a8129f34520daf3977048b08adf99dd6cced07
Dang Quang Vi <vidangquang1203@gmail.com>
2024-01-03 07:07:43 +0700

    Update custom_login.dart

--- qldt_pka/lib/view/login/custom_login.dart
+++ qldt_pka/lib/view/login/custom_login.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'login_view.dart';
-import 'package:flutter'
 
 Future<Object?> customSigninDialog(BuildContext context,
     {required ValueChanged onClosed}) {
